f26007488259769a6ce0191da9c5fcd6b1a70378
Elvis Arturo Tezen Villanueva <elvis.tezen@softtek.com>
2022-06-18 17:29:58 -0500

    creacion de pipe para ruta imagen

---
+++ src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroe.interface';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html',
+  styles: [
+  ]
+})
+export class HeroeTarjetaComponent implements OnInit {
+  @Input() heroe!: Heroe;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
